b54507cf455faaa52b3278dcf90616dc15b01497
mainshum <doseit@gmail.com>
2023-09-19 13:51:06 +0200

    use iphone 12 viewport

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    viewportHeight: 844,
+    viewportWidth: 390,
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
